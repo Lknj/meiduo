4f2ea4c024abc0275f6867b78fbcc87880454db3
Lknj <317362712@qq.com>
2018-12-05 16:09:15 +0800

    查询用户名个数,判断用户名是否存在

--- demo/demo/apps/users/views.py
+++ demo/demo/apps/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import User
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UsernameCountView(APIView):
+    # 查询用户名个数,判断用户名是否存在
+    def get(self, request, username):
+        count = User.objects.filter(username=username).count()
+        # 用户名存在, 响应
+        return Response({
+            "username": username,
+            "count": count
+        })
--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('^', include('verifycations.urls')),
+    url('^', include('users.urls')),
 ]
